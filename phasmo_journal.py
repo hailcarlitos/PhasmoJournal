89c595bb8d7e1b7630833896cb5be31db0f77077
Carlos <c.asun93@gmail.com>
2021-01-11 16:22:53 -0800

    Added back ground and consolidated evidence frames

--- phasmo_journal.py
+++ phasmo_journal.py
@@ -1,13 +1,20 @@
 
 import tkinter as tk
+from pathlib import Path
 
 ghost_evidence_list = ["None", "EMF Level 5", "FingersPrints", "Freezing Temperature", "Ghost Orbs", "Ghost Writing", "Sprit Box"]
 
 window = tk.Tk()
+
+background_image = tk.PhotoImage(file = "images/paper-texture.png")
+background_label = tk.Label(window, image=background_image)
+background_label.place(x=0, y=0, relwidth=1, relheight=1)
+
 window.title("Evidence Journal")
-window.geometry("350x500")
+window.geometry("350x400")
 tk.Label(window, text="Evidence", font=("Helvetica", 24)).pack()
 
+# Ghost Name
 ghost_name_frame = tk.Frame(window)
 
 tk.Label(ghost_name_frame, text="Ghost's Name", font=("Helvetica", 16)).pack()
@@ -16,34 +23,41 @@ ghost_name_box.pack()
 
 ghost_name_frame.pack(pady=10) # Can give these pady and padx to move in frame
 
-evidence_1_frame = tk.Frame(window)
+# Evidence
+evidence_frame = tk.Frame(window)
 
-tk.Label(evidence_1_frame, text="Evidence Found #1", font=("Helvetica", 16)).pack()
-evidence_1 = tk.StringVar(evidence_1_frame)
-evidence_1.set("None") # Default
-evidence_1_options = tk.OptionMenu(evidence_1_frame, evidence_1, *ghost_evidence_list)
-evidence_1_options.pack()
+tk.Label(evidence_frame, text="Evidence Found #1", font=("Helvetica", 16)).pack()
+evidence_1 = tk.StringVar(evidence_frame)
+evidence_1.set(ghost_evidence_list[0]) # Default
+evidence_1_options = tk.OptionMenu(evidence_frame, evidence_1, *ghost_evidence_list)
+evidence_1_options.pack(pady=10)
 
-evidence_1_frame.pack(pady=10)
+tk.Label(evidence_frame, text="Evidence Found #2", font=("Helvetica", 16)).pack()
+evidence_2 = tk.StringVar(evidence_frame)
+evidence_2.set(ghost_evidence_list[0]) # Default
+evidence_2_options = tk.OptionMenu(evidence_frame, evidence_2, *ghost_evidence_list)
+evidence_2_options.pack(pady=10)
 
-evidence_2_frame = tk.Frame(window)
+tk.Label(evidence_frame, text="Evidence Found #3", font=("Helvetica", 16)).pack()
+evidence_3 = tk.StringVar(evidence_frame)
+evidence_3.set(ghost_evidence_list[0]) # Default
+evidence_3_options = tk.OptionMenu(evidence_frame, evidence_3, *ghost_evidence_list)
+evidence_3_options.pack(pady=10)
 
-tk.Label(evidence_2_frame, text="Evidence Found #2", font=("Helvetica", 16)).pack()
-evidence_2 = tk.StringVar(evidence_2_frame)
-evidence_2.set("None") # Default
-evidence_2_options = tk.OptionMenu(evidence_2_frame, evidence_2, *ghost_evidence_list)
-evidence_2_options.pack()
+evidence_frame.pack()
 
-evidence_2_frame.pack(pady=10)
+# Clear Function
+def clear():
+    ghost_name_box.delete(1.0, tk.END)
+    evidence_1.set(ghost_evidence_list[0])
+    evidence_2.set(ghost_evidence_list[0])
+    evidence_3.set(ghost_evidence_list[0])
 
-evidence_3_frame = tk.Frame(window)
+# Button
+button_frame = tk.Frame(window)
+button_frame.pack()
 
-tk.Label(evidence_3_frame, text="Evidence Found #3", font=("Helvetica", 16)).pack()
-evidence_3 = tk.StringVar(evidence_3_frame)
-evidence_3.set("None") # Default
-evidence_3_options = tk.OptionMenu(evidence_3_frame, evidence_3, *ghost_evidence_list)
-evidence_3_options.pack()
-
-evidence_3_frame.pack(pady=10)
+clear_button = tk.Button(button_frame, text="Clear", font=("Helvetica", 12), command=clear)
+clear_button.pack()
 
 window.mainloop()
